8d68a5f7f4392e323b084b02a8002a0409656e46
shuvosonjoy <shuvosdg123@gmail.com>
2024-06-21 20:31:54 +0600

    headers

--- EXPRESS/src/controllers/DemoControllers.js
+++ EXPRESS/src/controllers/DemoControllers.js
@@ -1,29 +1,29 @@
-exports.demo=(req,res)=>{
-let userName=req.params.name;
-let userAge = req.params.age;
-res.status(200).json({
+
+exports.demo = (req, res) => {
+  let userName = req.params.name;
+  let userAge = req.params.age;
+  res.status(200).json({
     name: userName,
-    age:userAge,
-})
-
-}
-
-exports.demo1=(req,res)=>{
-
-let name= req.body['name'];
-let age=req.body['age'];
-let email=req.body['email'];
-res.status(200).json({
-    name:name,
-    age:age,
-    email:email,
-
-})
-
-   
-    }
+    age: userAge,
+  });
+};
 
 
-exports.demo2=(req,res)=>{
-        res.send('message is from demo2');
-        }
+exports.demo1 = (req, res) => {
+  let name = req.body["name"];
+  let age = req.body["age"];
+  let email = req.body["email"];
+  res.status(200).json({
+    name: name,
+    age: age,
+    email: email,
+  });
+};
+
+
+exports.demo2 = (req, res) => {
+  let token = req.headers['token'];
+  res.status(200).json({
+"tokens":token
+  })
+};
--- EXPRESS/src/routes/api.js
+++ EXPRESS/src/routes/api.js
@@ -6,7 +6,7 @@ const Democontroller = require('../controllers/DemoControllers');
 
 router.get('/demo/:name/:age',Democontroller.demo)
 router.post('/demo1',Democontroller.demo1)
-router.delete('/demo2',Democontroller.demo2)
+router.post('/demo2',Democontroller.demo2)
 
 
 
