db87aedfee64e5e652b38cb0d6012a59a46fbbf2
shuvosonjoy <shuvosdg123@gmail.com>
2024-06-23 15:41:12 +0600

    sort of packages

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const app = express();
 const router = require('./src/routes/api');
 const cookieParser = require('cookie-parser');
-const app = express();
-require('./src/routes/api')
+const cors = require('cors');
+const hpp = require('hpp')
+const helmet = requier('helmet');
+const sanitizerPlugin = require('mongoose-sanitizer')
+const rateLimit = require('express-rate-limit')
+
+const limiter=rateLimit({windowMs:15*60*1000,max:100})
 
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
+app.use(hpp());
+app.use(sanitizerPlugin());
+app.use(helmet());
+app.use(limiter);
+
+
 app.use('/api',router);
 
 
-
-module.exports=app;
+module.exports=app;f
