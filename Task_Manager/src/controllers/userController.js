1f15ff8f49585ed8b6120c7da54c7ef9a639ce0c
shuvosonjoy <shuvosdg123@gmail.com>
2024-06-25 20:19:55 +0600

    login & registration

--- Task_Manager/src/controllers/userController.js
+++ Task_Manager/src/controllers/userController.js
@@ -1,12 +1,35 @@
-const UserModel = require('../models/usermodels');
-const jwt = require('jsonwebtoken');
+const UserModel = require("../models/usermodels");
+const jwt = require("jsonwebtoken");
 
 exports.registration = async (req, res) => {
-    let reqBody = req.body;
-    try {
-        let result = await UserModel.create(reqBody);
-        res.status(200).json({ status: "success", data: result });
-    } catch (e) {
-        res.status(200).json({ status: "fail", data:e });
+  let reqBody = req.body;
+  try {
+    let result = await UserModel.create(reqBody);
+    res.status(200).json({ status: "success", data: result });
+  } catch (e) {
+    res.status(200).json({ status: "fail", data: e });
+  }
+};
+
+
+
+exports.login = async (req, res) => {
+  let reqBody = req.body;
+  try {
+    let result = await UserModel.find(reqBody).count();
+
+    if (result === 1) {
+      res.status(200).json({
+        status: "success",
+        data: result,
+      });
+    } else {
+      res.status(200).json({
+        status: "failed",
+        data: "data not found",
+      });
     }
+  } catch (e) {
+    res.status(200).json({ status: "fail", data: e });
+  }
 };
